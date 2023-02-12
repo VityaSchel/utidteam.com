70c4b7c4a7a11ca6067c6cb9da39e4ceb12d3d37
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-02-13 00:50:07 +0400

    Add i18n

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  i18n
 }
 
 module.exports = nextConfig
